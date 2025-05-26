edb813f40abd257997be5f6c776d9019c726d2d0
arthur <arthur.o.f.miranda2011@gmail.com>
2025-05-26 12:04:45 -0300

    feat: create user_model

---
+++ equigest/models/user.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from equigest.infra.database import mapped_registry
+
+@mapped_registry.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), init=False, default=func.now()
+    )
