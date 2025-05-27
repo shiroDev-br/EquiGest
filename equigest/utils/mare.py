31a118f90a858e3496ca6f1a0fad548e07a68633
arthur <arthur.o.f.miranda2011@gmail.com>
2025-05-27 12:11:30 -0300

    feat: create mare utils for managment calendar

---
+++ equigest/utils/mare.py
@@ -0,0 +1,39 @@
+from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta
+
+def get_birth_forecast(
+    pregnancy_date: datetime
+) -> datetime:
+    return pregnancy_date + timedelta(days=335)
+
+def get_p4_schedule(
+    pregnancy_date: datetime
+) -> list[datetime]:
+    schedule = []
+
+    current_date = pregnancy_date
+    end_date = pregnancy_date + timedelta(days=105)
+
+    while current_date <= end_date:
+        schedule.append(current_date)
+        current_date += timedelta(days=15)
+    
+    return schedule
+
+def get_herpes_vaccine_schedule(
+    pregnancy_date: datetime
+) -> list[datetime]:
+    return [
+        pregnancy_date + relativedelta(months=5),
+        pregnancy_date + relativedelta(months=7),
+        pregnancy_date + relativedelta(months=9)
+    ]
+
+def get_managment_schedule(
+    pregnancy_date: datetime
+) -> dict[str, list[datetime]]:
+    return {
+        'Herpes Vaccine': get_herpes_vaccine_schedule(pregnancy_date),
+        "Birth Forecast": get_birth_forecast(pregnancy_date),
+        "P4": get_p4_schedule(pregnancy_date),
+    }
