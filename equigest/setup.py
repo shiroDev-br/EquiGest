06982662b0f29435217e49908d0c03c24a3c738a
arthur <arthur.o.f.miranda2011@gmail.com>
2025-05-30 21:08:28 -0300

    feat: implementing the limiter on application

--- equigest/setup.py
+++ equigest/setup.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_adress
+
+limiter = Limiter(key_func=get_remote_adress)
+
 def setup_app():
     app = FastAPI(
         title='EquiGest',
         description='A mare management service',
         version='0.1.0'
     )
+
+    app.state.limiter = limiter
+    app.add_exception_handler(429, _rate_limit_exceeded_handler)
 
     from equigest.routers.auth import auth_router
     from equigest.routers.mare import mare_router
