657e187ec8cf6fb3080569b9ab51cb52324ab5d1
arthur <arthur.o.f.miranda2011@gmail.com>
2025-05-30 21:11:00 -0300

    fix: fix import error of get_remote_adress

--- equigest/setup.py
+++ equigest/setup.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
 from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_adress
+from slowapi.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_adress)
+limiter = Limiter(key_func=get_remote_address)
 
 def setup_app():
     app = FastAPI(
