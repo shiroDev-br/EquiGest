f8f11d506322c55c9b420b35e71eefd1f43f8cf9
arthur <arthur.o.f.miranda2011@gmail.com>
2025-06-07 10:02:54 -0300

    refactor: add next_payment_date in UserCreateSchema

--- equigest/schemas/user.py
+++ equigest/schemas/user.py
@@ -1,9 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from datetime import datetime, timedelta
 
 
 class UserCreateSchema(BaseModel):
     username: str
     password: str
+    next_payment_date: datetime = Field(default_factory=lambda: datetime.utcnow() + timedelta(days=7))
 
 class UserSchema(BaseModel):
     id: int
