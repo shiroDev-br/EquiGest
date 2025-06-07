c3cb34f397eadb77d40f1819ce14ec57d85a5884
arthur <arthur.o.f.miranda2011@gmail.com>
2025-06-07 09:22:30 -0300

    feat and refactor: create PaymentAccessStatus Enum and add payment_status and next_payment_date in usermodel

--- equigest/models/user.py
+++ equigest/models/user.py
@@ -1,9 +1,13 @@
+from typing import Optional
+
 from datetime import datetime
 
-from sqlalchemy import DateTime, func
+from sqlalchemy import DateTime, func, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 
 from equigest.infra.database import mapper_registry
+
+from equigest.enums.enums import PaymentAccessStatus
 
 @mapper_registry.mapped_as_dataclass
 class User:
@@ -12,6 +16,12 @@ class User:
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
+    payment_status: Mapped[PaymentAccessStatus] = mapped_column(
+        Enum(PaymentAccessStatus), default=PaymentAccessStatus.TRIAL
+    )
+    next_payment_date: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), nullable=True
+    )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), init=False, default=func.now()
     )
