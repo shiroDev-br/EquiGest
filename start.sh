b13a4e0c2b37a4960e84300fda4468ee532f7c4c
arthur <arthur.o.f.miranda2011@gmail.com>
2025-06-08 12:16:14 -0300

    tryfix: trying to fix celery hearbeat error

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 uvicorn equigest.app:app --host 0.0.0.0 --port 8000 --reload &
-celery -A equigest.celery.celery_app worker --loglevel=debug --pool=solo -E
+celery -A equigest.celery.celery_app worker --loglevel=debug --without-heartbeat --pool=solo -E
 
 wait -n
 
