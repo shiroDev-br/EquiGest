dc5280299a49bbe514fc89f30d2b2d79d5cb8e45
arthur <arthur.o.f.miranda2011@gmail.com>
2025-06-18 21:53:32 -0300

    chore: add mare_type in MareQueryParams

--- equigest/schemas/query.py
+++ equigest/schemas/query.py
@@ -3,12 +3,14 @@ from datetime import date
 
 from fastapi import Query
 
+from equigest.enums.enums import MareType
 
 class MareQueryParams(BaseModel):
     start_date: date = Query(..., description="Data inicial do intervalo")
     end_date: date = Query(..., description="Data final do intervalo")
     page: int = Query(1, ge=1, description="Número da página")
     size: int = Query(10, ge=1, le=100, description="Itens por página")
+    mare_type: MareType = None
 
 class MareQueryByBirthForecastParams(BaseModel):
     mare_type: str
