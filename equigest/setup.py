71e2b02329273de4c92a3faebf44de6fe879282c
arthur <arthur.o.f.miranda2011@gmail.com>
2025-06-22 20:03:36 -0300

    style: remove a random space

--- equigest/setup.py
+++ equigest/setup.py
@@ -4,7 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi_pagination import add_pagination
 
-
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 
